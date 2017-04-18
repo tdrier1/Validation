39ec5c36c9d40f3d6c6eb995ee8ef2f022f49ba4
tdrier1 <tdrier1@gmail.com>
2017-04-18 11:02:10 -0400

    Uploading doc, work still needed

--- Validation_Bonus/Program.cs
+++ Validation_Bonus/Program.cs
@@ -10,15 +10,24 @@ namespace Validation_Bonus
     {
         static void Main(string[] args)
         {
-            string S1, S2;
-            int Min, Max, rvalue;
-            bool CaseSensitive;
+            double S1 = 1;
+            double S2 = 2;
+            string S3 = "Test1";
+            string S4 = "Test2";
+            int Min = 1;
+            int Max = 100;
+            double value;
+            bool CaseSensitive = true;
 
             Console.Write("Please enter a value: ");
             string Prompt = Console.ReadLine();
 
-           
-            
+            GetInt();
+            GetIntWithinRange(Prompt, Min, Max);
+            GetDouble(Prompt);
+            GetDoubleWithinRange(Prompt, Min, Max);
+            GetChoiceStringOne(Prompt, S1, S2);
+            GetChoiceStringTwo(Prompt, S3, S4, CaseSensitive);
         }
 
         /* This method will return a valid integer. This method takes a string prompt that is used to ask the user for an integer.*/
@@ -46,7 +55,6 @@ namespace Validation_Bonus
                     Console.WriteLine("\n");
                     Console.Write(Prompt);
                 }
-
             }
             return value;
         }
@@ -55,10 +63,7 @@ namespace Validation_Bonus
         public static int GetIntWithinRange(string Prompt, int Min, int Max)
         {
             int value;
-
-            Random rnd = new Random();
-            int num = rnd.Next(Min, Max);
-            while (true)
+            while(true)
             {
                 if (!Int32.TryParse(Console.ReadLine(), out value))
                 {
@@ -67,19 +72,17 @@ namespace Validation_Bonus
                         Console.Write("Please enter a valid input:");
                     }
                 }
-                if (num < Min || num > Max)
+                if (value < Min || value > Max)
                 {
                     {
                         Console.WriteLine("\n");
                         Console.Write("Please enter a valid input:");
                         continue;
                     }
-
                 }
                 return value;
             }
         }
-
 
         /* This method will return a valid double. This method takes a string prompt that is used to ask the user for a double.*/
         public static double GetDouble(string Prompt)
@@ -92,20 +95,14 @@ namespace Validation_Bonus
                     Console.WriteLine("\n");
                     Console.Write("Try again! Please enter an integer: ");
                 }
-
             }
             return dblvalue;
         }
-
-
 
         /* This method will return a valid integer that is within a certain range(min and max). This method also takes a string prompt that is used to ask the user for an integer.*/
         public static double GetDoubleWithinRange(string Prompt, int Min, int Max)
         {
             double value;
-
-            Random rnd = new Random();
-            double num = rnd.Next(Min, Max);
 
             while (true)
             {
@@ -116,41 +113,66 @@ namespace Validation_Bonus
                         Console.Write("Please enter a valid input:");
                     }
                 }
-                if (num < Min || num > Max)
+                if (value < Min || value > Max)
                 {
                     {
                         Console.WriteLine("\n");
                         Console.Write("Please enter a valid input:");
                         continue;
                     }
-
                 }
                 return value;
             }
         }
 
+        /* This method will validate the user’s input against two choices(S1 or S2). This method takes a string prompt that is used to ask the user for a double.*/
+        public static double GetChoiceStringOne(string Prompt, double S1, double S2)
+        {
+            double value;
+            bool run = true;
 
-        ///* This method will validate the user’s input against two choices(S1 or S2). This method takes a string prompt that is used to ask the user for a double.*/
-        //public static string GetChoiceString(string Prompt, string S1, string S2)
-        //{
-        //    double value;
+           
+                if (!Double.TryParse(Console.ReadLine(), out value))
+                {
+                    Console.WriteLine("\n");
+                    Console.Write("Try again! Please enter an integer: ");
+                }
+                if  (value != S2 || value != S1)
+                {
+                    Console.WriteLine("Please try again!");
+                }
+                if (value == S1 || value == S2)
+                {
+                    run = false;
+                }
+            
+            return value;
+        }
 
-        //    while (!Double.TryParse(Console.ReadLine(), out value))
-        //    {
-        //        {
-        //            Console.WriteLine("\n");
-        //            Console.Write("Try again! Please enter an integer: ");
-        //        }
+        /* This method will validate the user’s input against two choices (S1 or S2). This method takes a string prompt that is used to ask the user for a double. You can choose to have the comparison case sensitive or not.*/
+        public static string GetChoiceStringTwo(string Prompt, string S3, string S4, bool CaseSensitive)
+        {
+            string value;
+            bool run = true;
+            
+            Console.WriteLine("Please enter a string: ");
 
-        //    }
-        //    double value;
-        //}
+            while (true)
+            {
+                value = Console.ReadLine();
+                
+                if (value != S3 || value != S4)
+                {
+                    Console.WriteLine("Please try again!");
+                }
 
-
-        ///* This method will validate the user’s input against two choices (S1 or S2). This method takes a string prompt that is used to ask the user for a double. You can choose to have the comparison case sensitive or not.*/
-        //public static string GetChoiceString(string Prompt, string S1, string S2, bool CaseSensitive)
-        //{
-
-        //}
+                if (value == S3 || value == S4)
+                {
+                    run = false;
+                }
+                return value;
+            }
+            
+        }
     }
 }
